b442e7b618da42bf23c36ea1fc26e53aad5984ee
s0lst1ce <cadmium-beaver@pm.me>
2026-06-28 15:44:29 +0200

    fix: get branch from tree & buttons activation state

--- branch.gd
+++ branch.gd
@@ -1,5 +1,5 @@
 extends Furniture
 
 func _interact_with(item: ItemData) -> void:
-	ItemExchange.add_item.emit("res://branch.tres")
+	ItemExchange.add_item.emit(preload("res://branch.tres"))
 	self.queue_free()
--- tree_lock.gd
+++ tree_lock.gd
@@ -1,5 +1,15 @@
 extends Furniture
 
+@onready var buttons = [$"../B1", $"../B2", $"../B3"]
+
+func _ready() -> void:
+	for button in buttons:
+		button.set_process(false)
+		button.hide()
 
 func _interact_with(item: ItemData) -> void:
+	for b in buttons:
+		var button = get_node(b)
+		button.set_process(true)
+		button.show()
 	self.queue_free()
