5e5c416dd8d73ad738194b92ebeeff3654a482ea
s0lst1ce <cadmium-beaver@pm.me>
2026-06-28 16:02:48 +0200

    fix: tree lock

--- tree_lock.gd
+++ tree_lock.gd
@@ -7,9 +7,8 @@ func _ready() -> void:
 		button.set_process(false)
 		button.hide()
 
-func _interact_with(item: ItemData) -> void:
-	for b in buttons:
-		var button = get_node(b)
+func _interact_with(_item: ItemData) -> void:
+	for button in buttons:
 		button.set_process(true)
 		button.show()
 	self.queue_free()
