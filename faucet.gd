dbb8a0f7a72a9c5306a7fc0b30776c011ed3b5a3
s0lst1ce <cadmium-beaver@pm.me>
2026-06-29 00:18:23 +0200

    fix: soft lock

--- faucet.gd
+++ faucet.gd
@@ -1,13 +1,14 @@
 extends Furniture
 
 @onready var repaired = preload("res://assets/furniture/faucet.png")
-
+@onready var filled_once = false
 
 func _interact_with(item: ItemData) -> void:
 	if item.name == "wrench" and texture != repaired:
 		texture = repaired
 		Enigmas.bedroom_flushed = true
-	elif item.name == "glass":
+	elif item.name == "glass" and !filled_once:
+		filled_once = true
 		ItemExchange.add_item.emit(preload("res://glass_water.tres"))
 	elif item.name == "kettle":
 		ItemExchange.add_item.emit(preload("res://boiling_water.tres"))
