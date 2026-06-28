16f8cc14e48f66209f72c697f6440409aa73acfb
s0lst1ce <cadmium-beaver@pm.me>
2026-06-29 00:57:58 +0200

    feat: add lights

--- plant_button.gd
+++ plant_button.gd
@@ -2,5 +2,9 @@ extends Furniture
 
 func _process(delta: float) -> void:
 	if Enigmas.plant_dead:
+		show()
+
+func _on_click() -> void:
+	if Enigmas.plant_dead:
 		ItemExchange.add_item.emit(preload("res://assets/furniture/shirt_button_orange.png"))
 		queue_free()
