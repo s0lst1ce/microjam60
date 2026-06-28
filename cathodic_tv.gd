25faa76cab8f1faad0697a61f6404674bec5ada1
s0lst1ce <cadmium-beaver@pm.me>
2026-06-29 01:36:24 +0200

    feat: lights

--- cathodic_tv.gd
+++ cathodic_tv.gd
@@ -1,7 +1,9 @@
 extends Sprite2D
 
 @onready var on_texture = preload("res://assets/furniture/squirrel/cathodic_tv_on.png")
+@onready var screen = $Screen
 
 func _process(_delta: float) -> void:
 	if Enigmas.tv_on and texture != on_texture:
 		texture = on_texture
+		screen.show()
