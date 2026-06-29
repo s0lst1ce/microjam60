2ddff2b9b8b5fd79a29175caf82c67961a124ac5
s0lst1ce <cadmium-beaver@pm.me>
2026-06-29 02:23:28 +0200

    feat: cutscenes

--- shirt_button_lock.gd
+++ shirt_button_lock.gd
@@ -4,10 +4,18 @@ extends Furniture
 @onready var orange = $ShirtButtonOrange
 @onready var purple = $ShirtButtonPurple
 
+@onready var filled = [false, false, false]
+
 func _interact_with(item: ItemData) -> void:
 	if item.name == "shirt_button_blue":
+		filled[0]=true
 		blue.show()
 	elif item.name == "shirt_button_orange":
+		filled[1]=true
 		orange.show()
 	elif item.name == "shirt_button_purple":
+		filled[2]=true
 		purple.show()
+
+	if filled == [true, true, true]:
+		Enigmas.has_won = true
