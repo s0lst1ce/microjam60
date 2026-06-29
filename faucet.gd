fb2fb9db59c879742acbcb8a96c7989079466b39
s0lst1ce <cadmium-beaver@pm.me>
2026-06-29 02:35:29 +0200

    fix: softlock

--- faucet.gd
+++ faucet.gd
@@ -7,8 +7,11 @@ func _interact_with(item: ItemData) -> void:
 	if item.name == "wrench" and texture != repaired:
 		texture = repaired
 		Enigmas.bedroom_flushed = true
-	elif item.name == "glass" and !filled_once:
-		filled_once = true
-		ItemExchange.add_item.emit(preload("res://glass_water.tres"))
+	elif item.name == "glass":
+		if filled_once:
+			ItemExchange.add_item.emit(preload("res://glass.tres"))
+		else:
+			filled_once = true
+			ItemExchange.add_item.emit(preload("res://glass_water.tres"))
 	elif item.name == "kettle":
 		ItemExchange.add_item.emit(preload("res://boiling_water.tres"))
