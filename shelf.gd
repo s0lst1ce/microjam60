21b1fc333c1ea72659139e57c1434c9e63ad5bc8
s0lst1ce <cadmium-beaver@pm.me>
2026-06-29 02:30:27 +0200

    fix: prevent softlock

--- shelf.gd
+++ shelf.gd
@@ -1,10 +1,12 @@
 extends Furniture
 
 @onready var glass = $EmptyGlass
+@onready var glass2 = $EmptyGlass2
 var has_glass=true
 
 func _process(delta: float) -> void:
 	if len(gives) == 0 && has_glass:
 		has_glass=false
 		self.remove_child(glass)
+		self.remove_child(glass2)
 		
