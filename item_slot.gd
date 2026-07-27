7b0fbcaa266cf911eb3d964a234a59c5de568f68
s0lst1ce <cadmium-beaver@pm.me>
2026-07-27 14:57:45 +0200

    feat: item highlight

--- item_slot.gd
+++ item_slot.gd
@@ -1,6 +1,7 @@
 extends Panel
 
 @onready var icon: TextureRect = $Icon
+@onready var item_highlight: ColorRect = $Icon/ItemHighlight
 @export var item: ItemData
 
 func _ready() -> void:
@@ -17,7 +18,7 @@ func update_ui():
 func _get_drag_data(_at_position: Vector2) -> Variant:
 	if not item:
 		return
-		
+
 	var preview = duplicate()
 	var c = Control.new()
 	c.add_child(preview)
@@ -30,10 +31,10 @@ func _get_drag_data(_at_position: Vector2) -> Variant:
 	set_drag_preview(c)
 	icon.hide()
 	return self
-	
+
 func _can_drop_data(_at_position: Vector2, _data: Variant) -> bool:
 	return true
-	
+
 func _drop_data(_at_position: Vector2, data: Variant) -> void:
 	if data.item != null and item !=null and data.item.combines_with == item.name:
 		item= item.combines_into
@@ -47,5 +48,6 @@ func _drop_data(_at_position: Vector2, data: Variant) -> void:
 	update_ui()
 	data.update_ui()
 	print("Dropped ", item, data.item)
-	
-	
+
+func _on_mouse_entered() -> void:
+	item_highlight.visible=false
